e94de4cac0e87b834028e1a8fb7a242df532f379
Calder Birdsey <cbirdsey@middlebury.edu>
2019-04-04 16:37:19 -0400

    adding landing page component and basic state in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LandingPage from './components/Landing';
+//import Uploader from './components/Uploader';
 
 /* eslint-disable react/prefer-stateless-function */
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      mode: 'landing'
+    };
+  }
+
   render() {
+    let content;
+    if (this.state.mode === 'landing') {
+      content = <LandingPage />;
+    } else if (this.state.mode === 'upload') {
+      //ignore for now, forward compatible with uploading page
+      //content = <Uploader />;
+      content = <LandingPage />;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to your CS312 Project</h1>
+          <h1 className="App-menu">Menu Placeholder</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {content}
       </div>
     );
   }
