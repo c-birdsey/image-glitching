4774c356432a70e52d04ee48f0a0ed158d91a1fd
Will Frazier <wjfrazier@middlebury.edu>
2019-04-15 21:07:41 -0400

    emails and passwords save but nothing happens with them

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Container,
+  Col
+} from 'reactstrap';
 
 class Login extends Component {
   constructor() {
@@ -7,44 +16,117 @@ class Login extends Component {
 
     this.state = {
       mode: 'login',
-      user: ''
+      email: '',
+      password: ''
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = async event => {
+    const { target } = event;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
+    await this.setState({
+      [name]: value
+    });
+  };
+
+  submitForm(e) {
+    e.preventDefault();
+    console.log(`Email: ${this.state.email}`);
+    console.log(`Password: ${this.state.password}`);
   }
 
   render() {
     if (this.state.mode === 'login') {
       return (
-        <Form>
-          <FormGroup>
-            <Label for="userEmail">Email</Label>
-            <Input
-              type="email"
-              name="email"
-              id="userEmail"
-              placeholder="Please enter your email"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="userPassword">Password</Label>
-            <Input
-              type="password"
-              name="password"
-              id="userPassword"
-              placeholder="Please enter your password"
-            />
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="checkbox" /> Remember Me
-            </Label>
-          </FormGroup>
-          <Button>Login</Button>
-          <br />
-          <br />
-          <br />
-          <br />
-          <a>Need an account?</a> <Button>Sign up</Button>
-        </Form>
+        <Container className="app">
+          <Form onSubmit={e => this.submitForm(e)}>
+            <FormGroup>
+              <Label for="userEmail">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="userEmail"
+                placeholder="Please enter your email"
+                onChange={e => this.handleChange(e)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="userPassword">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="userPassword"
+                placeholder="Please enter your password"
+                onChange={e => this.handleChange(e)}
+              />
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="checkbox" /> Remember Me
+              </Label>
+            </FormGroup>
+            <Button>Login</Button>
+            <br />
+            <br />
+            <br />
+            <br />
+            <a>Need an account?</a>{' '}
+            <Button onClick={() => this.setState({ mode: 'register' })}>
+              Sign up
+            </Button>
+          </Form>
+        </Container>
+      );
+    }
+    if (this.state.mode === 'register') {
+      return (
+        <Container className="app">
+          <Form>
+            <FormGroup>
+              <Label for="userEmail">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="userEmail"
+                placeholder="Please enter your email"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="userPassword">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="userPassword"
+                placeholder="Please enter your password"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="userPassword">Confirm Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="confirmPassword"
+                placeholder="Please re-enter your password"
+              />
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="checkbox" /> Remember Me
+              </Label>
+            </FormGroup>
+            <Button>Login</Button>
+            <br />
+            <br />
+            <br />
+            <br />
+            <a>Already have an account?</a>{' '}
+            <Button onClick={() => this.setState({ mode: 'login' })}>
+              Login
+            </Button>
+          </Form>
+        </Container>
       );
     }
   }
