7a93b6be4366edf7417aad4231269aff0d12455d
trey <treyoehmler@gmail.com>
2019-05-06 19:28:45 -0400

    added basic relational mappings for Images and Users

--- server/migrations/20190503215203_load-images.js
+++ server/migrations/20190503215203_load-images.js
@@ -1,12 +1,16 @@
 /* eslint-disable func-names */
 /* eslint no-unused-vars: ["error", { "args": "none" }] */
 
-// TODO: Add relations to users & og / glitched images
-
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('Images', table => {
     table.increments('id').primary();
     table.binary('data');
+    table.string('createdAt').notNullable();
+    table.integer('createdBy');
+    table
+      .foreign('createdBy')
+      .references('User.id')
+      .onDelete('SET NULL');
   });
 };
 
