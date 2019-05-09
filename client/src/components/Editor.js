b357768bc9ab7553611b33dda6f939e1e7fd534b
Guanghan Pan <gpan@middlebury.edu>
2019-05-08 20:48:03 -0400

    fix linting issues for annotation page

--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
 class Editor extends Component {
