5b395097a221ecd4b8ae1513dd1bda74b6eb70a3
Will Frazier <wjfrazier@middlebury.edu>
2019-05-11 17:07:19 -0400

    fixes for Travis

--- client/src/components/documentation.test.js
+++ client/src/components/documentation.test.js
@@ -5,17 +5,15 @@ import Documentation from './Documentation';
 describe('Documentation test', () => {
   describe('Documentation Page', () => {
     let doc;
-    let status;
     const completeCallback = jest.fn();
     beforeEach(() => {
       completeCallback.mockReset();
-      status = false;
       doc = mount(<Documentation />);
     });
 
     test('Displays', () => {
       const text = doc.find('#documentation');
-      expect(doc.exists()).toBe(true);
+      expect(text.exists()).toBe(true);
     });
   });
 });
