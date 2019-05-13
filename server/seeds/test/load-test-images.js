016368dfd771dc52568bb9238cae98f8a0890cc3
trey <treyoehmler@gmail.com>
2019-05-13 04:14:03 -0400

    updated server tests

--- server/seeds/test/load-test-images.js
+++ server/seeds/test/load-test-images.js
@@ -10,15 +10,10 @@ exports.seed = function(knex, promise) {
           id: 1,
           url:
             'http://res.cloudinary.com/drwjeyjxc/image/upload/v1557273995/images/swxori8eyzaatqwjirgc.png',
+          original:
+            'http://res.cloudinary.com/drwjeyjxc/image/upload/v1557729426/images/moinwhljt8j2wz7v8lyk.jpg',
           createdAt: '2019-05-08T00:06:35Z',
           createdBy: 1
-        },
-        {
-          id: 2,
-          url:
-            'http://res.cloudinary.com/drwjeyjxc/image/upload/v1557275130/images/zv6nmcyqtn8uvozdxnwt.png',
-          createdAt: '2019-05-08T00:25:30Z',
-          createdBy: 2
         }
       ])
     );
